ec3255c044e4b65cde18282a35e3651bfd542bb2
Mayan <gabrielmayan@gmail.com>
2022-01-30 21:00:32 -0300

    Adção de trycatck em getProducts

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,12 +1,16 @@
 const { getInfoPaginated } = require('../helpers/handleKnex');
 
 const getProducts = (request, response) => {
-  const { page, pageSize } = request.params;
+  try {
+    const { page, pageSize } = request.params;
 
-  const conditions = { deletedAt: false };
-  const products = getInfoPaginated('products', conditions, page, pageSize);
+    const conditions = { deletedAt: false };
+    const products = getInfoPaginated('products', conditions, page, pageSize);
 
-  return response.status(200).json(products);
+    return response.status(200).json(products);
+  } catch (error) {
+    return response.status(400).json('Não existem produtos em estoque');
+  }
 };
 
 module.exports = { getProducts };
